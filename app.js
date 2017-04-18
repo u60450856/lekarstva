c888910c2421e193b9acd9bf5c4ec207a97b5f0c
u60450856 <vc1983@mail.ru>
2017-04-18 23:12:41 +0300

    code fix

--- app.js
+++ app.js
@@ -1,49 +1,49 @@
-var APP = (function(init){
+getXmlHttp = function() {
   'use strict';
-  var _items = {};
-  var _options = {
-	"dataUrl": "/data.json",
-  };
-  var _onload = function(event){
-        document.removeEventListener('DOMContentLoaded', _onload);
-        _getData();
-  };
-  var _loadData = function (data) { _items = json.parse(data); };
-  // var _loadData = (data) => { _items = json.parse(data); };  
-  var _getData = function (){
-  	var xmlhttp = getXmlHttp(){
-		xmlhttp.open('GET', _options.dataUrl, true);
-		xmlhttp.onreadystatechange = function(){
-		  if (xmlhttp.readyState == 4){
-		    if(xmlhttp.status == 200){
-		      _loadData();
-		    }
-		  }
-		};
-		xmlhttp.send(null);
-    }
-  };
-  var APP = {
-    init   : function() { document.addEventListener('DOMContentLoaded', _onload); }
-  };
-  if (init && (false === APP.init())){ return null; } 
-  return APP;
-}(true));
-
-//**********************************************
-getXmlHttp = function(){
   var xmlhttp;
   try {
-    xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
+    xmlhttp = new ActiveXObject('Msxml2.XMLHTTP');
   } catch (e) {
     try {
-      xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+      xmlhttp = new ActiveXObject('Microsoft.XMLHTTP');
     } catch (E) {
       xmlhttp = false;
     }
   }
-  if (!xmlhttp && typeof XMLHttpRequest!='undefined') {
+  if (!xmlhttp && typeof XMLHttpRequest !== 'undefined') {
     xmlhttp = new XMLHttpRequest();
   }
   return xmlhttp;
 };
+// **********************************************
+var APP = (function(init) {
+  'use strict';
+  var _items = {};
+  var _options = {
+        'dataUrl': '/data.json',
+      };
+  var _onload = function(event) {
+    document.removeEventListener('DOMContentLoaded', _onload);
+    _getData();
+  };
+  var _loadData = function (data) { _items = json.parse(data); console.log(_items);};
+  // var _loadData = (data) => { _items = json.parse(data); };
+  var _getData = function () {
+    var xmlhttp = getXmlHttp();
+    xmlhttp.open('GET', _options.dataUrl, true);
+    xmlhttp.onreadystatechange = function() {
+      if (xmlhttp.readyState == 4){
+        if (xmlhttp.status == 200){
+          _loadData();
+        }
+      }
+    };
+    xmlhttp.send(null);
+  };
+  var APP = {
+    init   : function() { document.addEventListener('DOMContentLoaded', _onload); }
+  };
+  if (init && (false === APP.init())){ return null; }
+  return APP;
+}(true));
+
